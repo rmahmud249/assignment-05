3748db1de41684ba1f40e1c9884cf22d68aeb4b1
rmahmud249 <rmahmud.seu.bd@gmail.com>
2025-08-29 09:37:41 +0600

    third commit

--- js/script.js
+++ js/script.js
@@ -9,6 +9,19 @@ button.addEventListener('click', function(){
     heartIcon.classList.toggle('text-red-500');
 
     count++;
-    heartCount.firstChild.textContent = count + " ";
+    heartCount.firstChild.textContent = count;
   });
 });
+
+document.querySelectorAll(".copyBtn").forEach((btn) => {
+    btn.addEventListener("click", function() {
+        let textToCopy = document.querySelector(".copyText").innerText;
+        navigator.clipboard.writeText(textToCopy);
+        let counterSpan = document.querySelector(".copyCount");
+        let currentText = counterSpan.innerText;
+        let currentNum = parseInt(currentText) || 0;
+        counterSpan.innerText =currentNum+1;
+    });
+});
+
+
