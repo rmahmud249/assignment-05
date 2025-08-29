aa98ec77948593ed5391c195369f731c9edcab1f
rmahmud249 <rmahmud.seu.bd@gmail.com>
2025-08-29 12:05:41 +0600

    6th commit

--- js/script.js
+++ js/script.js
@@ -26,5 +26,50 @@ document.querySelectorAll(".copyBtn").forEach((btn) => {
     });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  let coins = 100;         
+  const callCost = 20;      
+  const coinBalance = document.querySelector(".coinBalance");
+  const historyList = document.getElementById("historyList");
+  const clearBtn = document.getElementById("clearHistory");
+  const callBtns = document.querySelectorAll(".item-btn .bg-green-500"); 
+  function updateCoinDisplay() {
+    coinBalance.childNodes[0].nodeValue = coins + " ";
+  }
+  callBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const card = btn.closest(".card");
+      const titleEl = card.querySelector("h3");
+      const numberEl = card.querySelector(".copyText");
 
+      if (!titleEl || !numberEl) return;
+      const title = titleEl.innerText;
+      const number = numberEl.innerText;
+      if (coins >= callCost) {
+        coins -= callCost;
+        updateCoinDisplay();
+        alert(`Calling ${title} ${number}`);
 
+        const li = document.createElement("li");
+        li.className = "p-2  rounded-md bg-gray-50";
+        li.innerHTML = `
+        <div class="flex justify-between items-center w-full">
+          <div class="flex flex-col">
+            <div class="font-semibold text-gray-900">${title}</div>
+            <div class="text-gray-600 font-semibold">${number}</div>
+          </div>
+          <div class="text-xs text-black">${new Date().toLocaleTimeString()}</div>
+        </div>
+      `;
+        historyList.appendChild(li);
+      } else {
+        alert("Not enough coins!");
+      }
+    });
+  });
+
+  clearBtn.addEventListener("click", () => {
+    historyList.innerHTML = "";
+  });
+  updateCoinDisplay();
+});
